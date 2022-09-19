bde6392b3e46f6dc466678ddf3b4fc1c4c83f603
Roy de Kleijn <dekleijn.roy@gmail.com>
2022-09-19 10:49:07 +0200

    log mime type

--- app.js
+++ app.js
@@ -224,7 +224,7 @@ function getResponseContent(entry) {
             break;
         case 'application/xml':
         case 'text/html':
-            return entry.response.content.text;
+            return entry.response.content.text.replace(/\</g,"&lt;").replace(/\>/g,"&gt;");
     }
 }
 
