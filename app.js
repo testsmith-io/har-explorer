8bd37b255e45b17ec22b028aed14eb86f0c01284
Testsmith <43567917+testsmith-io@users.noreply.github.com>
2022-10-24 21:43:23 +0200

    Update app.js

--- app.js
+++ app.js
@@ -224,7 +224,7 @@ function getResponseContent(entry) {
             break;
         case 'application/xml':
         case 'text/html':
-            return '<![CDATA['.entry.response.content.text.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').']]';
+            return entry.response.content.text.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
     }
 }
 
